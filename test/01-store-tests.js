308e6ccf8ddf2840b186713820e3efb2759f44e6
vigan-abd <vigan.abd@gmail.com>
2023-02-11 12:33:34 +0100

    fixed tests

--- test/01-store-tests.js
+++ test/01-store-tests.js
@@ -10,6 +10,7 @@ const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
 describe('ThriveCoinRewardSeason', () => {
   contract('store tests', (accounts) => {
     let contract
+    const season = 1
 
     before(async () => {
       contract = await ThriveCoinRewardSeason.deployed()
@@ -18,7 +19,7 @@ describe('ThriveCoinRewardSeason', () => {
     it('addReward should store new receipt', async () => {
       const userReward = { owner: accounts[0], destination: accounts[0], amount: '5' }
       await contract.addReward(userReward, { from: accounts[0] })
-      const reward = await contract.readReward(accounts[0])
+      const reward = await contract.readReward(season, accounts[0])
 
       assert.strictEqual(reward.destination, accounts[0])
       assert.strictEqual(reward.amount, '5')
@@ -32,23 +33,23 @@ describe('ThriveCoinRewardSeason', () => {
       ]
       await contract.addRewardBatch(userRewards, { from: accounts[0] })
 
-      let reward = await contract.readReward(accounts[0])
+      let reward = await contract.readReward(season, accounts[0])
       assert.strictEqual(reward.destination, accounts[0])
       assert.strictEqual(reward.amount, '3')
 
-      reward = await contract.readReward(accounts[1])
+      reward = await contract.readReward(season, accounts[1])
       assert.strictEqual(reward.destination, accounts[2])
       assert.strictEqual(reward.amount, '4')
     })
 
     it('readReward should return 0 address and amount when no reward exists', async () => {
-      const reward = await contract.readReward(accounts[2])
+      const reward = await contract.readReward(season, accounts[2])
       assert.strictEqual(reward.destination, ADDRESS_ZERO)
       assert.strictEqual(reward.amount, '0')
     })
 
     it('readReward should return entry when it exists', async () => {
-      const reward = await contract.readReward(accounts[0])
+      const reward = await contract.readReward(season, accounts[0])
       assert.strictEqual(reward.destination, accounts[0])
       assert.strictEqual(reward.amount, '3')
     })
